ab9ca42e8bf215333c0b1dd7f2ca5ef1d2a892df
Matthew Gream <matthew.gream@pobox.com>
2025-09-04 18:48:35 +0100

    updated

--- tools/ais-parser.sh
+++ tools/ais-parser.sh
@@ -1,1 +1,18 @@
-./ais-parser.js /opt/storage/collector/messages/* --display=9100 --min-distance=3 --exclude=2320752,111232029,111232031 --apikey=`cat .apikey`
+#!/bin/bash
+
+# Default values (non-full mode)
+DISPLAY_ARG="--display=9100"
+MIN_DISTANCE_ARG="--min-distance=3"
+
+# Check if --full flag is provided
+if [ "$1" = "--full" ]; then
+    DISPLAY_ARG=""
+    MIN_DISTANCE_ARG=""
+fi
+
+# Run the command with appropriate arguments
+./ais-parser.js /opt/storage/collector/messages \
+    ${DISPLAY_ARG} \
+    ${MIN_DISTANCE_ARG} \
+    --exclude=`cat ais-parser.exclude` \
+    --apikey=`cat .apikey`
